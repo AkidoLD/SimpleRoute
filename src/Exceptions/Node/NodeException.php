31d6eeafd5aa698ad877a78dd0d01b1fdcb1ef51
AkidoLD <alexrayan14231423@gmail.com>
2025-10-01 00:45:03 +0100

    chore : remove commented exception on NodeException file

--- src/Exceptions/Node/NodeException.php
+++ src/Exceptions/Node/NodeException.php
@@ -3,9 +3,3 @@
 namespace SimpleRoute\Exceptions\Node;
 
 class NodeException extends \Exception {}
-
-class NodeChildIsNotANode extends NodeException {}
-
-// class NodeHandlerNotSet extends NodeException {}
-
-// class NodeChildKeyMismatchException extends NodeException {}
